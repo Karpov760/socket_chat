9cc3756370af5813ac89e4d8f470f71385aaa425
Karpov760 <77189625+Karpov760@users.noreply.github.com>
2022-05-13 17:48:20 +0500

    Add files via upload

---
+++ client.py
@@ -0,0 +1,12 @@
+import socket
+import time
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+server.settimeout(0.2)
+message = b"code_frase"
+
+server.sendto(message, ('<broadcast>', 37020))
+data, addr = server.recvfrom(1024)
+print("received message: %s" % data)
