3ba8f61b838d024f7449d8c660c025119ad486af
Karpov760 <77189625+Karpov760@users.noreply.github.com>
2022-05-16 11:16:29 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
 import socket
 
+CODE = b"code_phrase"
+HOST = bytes(socket.gethostbyname(socket.gethostname()), 'utf-8')
+
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) # UDP
 client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 client.bind(("", 37020))
+
+
 
 
 while True:
@@ -13,4 +18,4 @@ while True:
     #если знакомый ip парсить бродкаст или ptp и работать как с чатом
     data, addr = client.recvfrom(1024)
     print("received message: %s"%data)
-    client.sendto(b'hello', addr)
+    client.sendto(HOST, addr)
